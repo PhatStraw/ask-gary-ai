e14af40a0e830475819687075544276e894a27a5
PhatStraw <kevindsimsjr@gmail.com>
2023-10-25 17:15:49 -0700

    clean-up

--- populate.js
+++ populate.js
@@ -6,58 +6,46 @@ const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
 const { VercelPostgres } = require("langchain/vectorstores/vercel_postgres");
 const { google } = require('googleapis');
 
+const channelId = 'UCIBgYfDjtWlbJhg--Z4sOgQ'; // Replace with the desired creator's ID
 
 
-const start = async () => {
+const config = {
+  postgresConnectionOptions: {
+    connectionString: process.env.NEXT_POSTGRES_URL
+  },
+};
+
+const vercelPostgresStore = await VercelPostgres.initialize(
+  new OpenAIEmbeddings({
+    openAIApiKey: process.env.NEXT_OPENAI_API_KEY
+  }),
+  config
+);
+
+// Function to get all video URLs from a specific creator
+async function getVideoUrlsFromCreator(channelId) {
   // Set up the YouTube Data API client
   const youtube = google.youtube({
     version: 'v3',
     auth: process.env.NEXT_YOUTUBE_DATA_API, // Replace with your own API key
   });
-
-  // Function to get all video URLs from a specific creator
-  async function getVideoUrlsFromCreator(channelId) {
-    try {
-      // Get all videos from the channel
-      const videosResponse = await youtube.search.list({
-        part: 'id',
-        channelId: channelId,
-        maxResults: 50, // Adjust the number of results as needed
-      });
-      const videoUrls = videosResponse.data.items.map((item) => `https://www.youtube.com/watch?v=${item.id.videoId}`);
-      return videoUrls;
-    } catch (error) {
-      console.error('Error retrieving video URLs:', error);
-      return [];
-    }
+  try {
+    // Get all videos from the channel
+    const videosResponse = await youtube.search.list({
+      part: 'id',
+      channelId: channelId,
+      maxResults: 50, // Adjust the number of results as needed
+    });
+    const videoUrls = videosResponse.data.items.map((item) => `https://www.youtube.com/watch?v=${item.id.videoId}`);
+    return videoUrls;
+  } catch (error) {
+    console.error('Error retrieving video URLs:', error);
+    return [];
   }
-  // Usage example
-  const channelId = 'UCIBgYfDjtWlbJhg--Z4sOgQ'; // Replace with the desired creator's username
-  const youtubeUrls = await getVideoUrlsFromCreator(channelId);
-  // Config is only required if you want to override default values.
-  const config = {
-    // tableName: "testvercelvectorstorelangchain",
-    postgresConnectionOptions: {
-      connectionString: process.env.NEXT_POSTGRES_URL
-    },
-    // columns: {
-    //   idColumnName: "id",
-    //   vectorColumnName: "vector",
-    //   contentColumnName: "content",
-    //   metadataColumnName: "metadata",
-    // },
-  };
+}
 
-
-  const vercelPostgresStore = await VercelPostgres.initialize(
-    new OpenAIEmbeddings({
-      openAIApiKey: process.env.NEXT_OPENAI_API_KEY 
-    }),
-    config
-  );
-
-
-  const loaders = youtubeUrls.map((url) => {
+const getLoaders = async (youtubeUrls) => {
+  return youtubeUrls.map((url) => {
     try {
       const hello = YoutubeLoader.createFromUrl(url, {
         language: 'en',
@@ -69,30 +57,38 @@ const start = async () => {
       return undefined; // Ignore the entry and continue with the next one
     }
   }).filter((loader) => loader !== undefined);
+}
 
-  
-  const documents = await Promise.all(loaders.map(async (loader) => {
+const loadnSplitDocuments = async (loaders) => {
+  return await Promise.all(loaders.map(async (loader) => {
     try {
       return await loader.loadAndSplit(
-          new CharacterTextSplitter({
-            separator: ' ',
-            chunkSize: 2500,
-            chunkOverlap: 200,
-          })
-        );
+        new CharacterTextSplitter({
+          separator: ' ',
+          chunkSize: 2500,
+          chunkOverlap: 200,
+        })
+      );
     } catch (error) {
       console.error('Error creating YoutubeLoader:', error);
       return undefined; // Ignore the entry and continue with the next one
     }
   }).filter((doc) => doc !== undefined))
+}
 
-  const ids = await Promise.all(documents.map(async (doc)=>{
-    if(doc) return await vercelPostgresStore.addDocuments(doc)
-    return 
+const start = async () => {
+  // Usage example
+  const youtubeUrls = await getVideoUrlsFromCreator(channelId);
+  const loaders = await getLoaders(youtubeUrls)
+  const documents = await loadnSplitDocuments(loaders)
+
+  const ids = await Promise.all(documents.map(async (doc) => {
+    if (doc) return await vercelPostgresStore.addDocuments(doc)
+    return
   }))
 
   const results = await vercelPostgresStore.similaritySearch("Where does Garry work?", 2);
-  console.log("result",results[0].metadata);
+  console.log("result", results[0].metadata);
   process.exit();
 }
 
