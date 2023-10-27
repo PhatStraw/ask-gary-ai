9031955139ef1e81715d6cee86e1649c7b1460fb
PhatStraw <kevindsimsjr@gmail.com>
2023-10-26 18:47:47 -0700

    clean-up

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ const ChatUI = () => {
             <button>
               <IconArrowRight
                 onClick={handleSubmit}
-                className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-blue-500 p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10 text-white"
+                className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-black p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10 text-white"
               />
             </button>
           </div>
