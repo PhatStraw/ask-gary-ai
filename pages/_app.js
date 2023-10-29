648bac66cd6fd21f2d61fd95ca38632d47f69f6b
PhatStraw <kevindsimsjr@gmail.com>
2023-10-28 23:35:20 -0700

    font

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,5 @@ import 'dotenv/config'
 import 'components/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (<div><Component {...pageProps} /></div>)
 }
